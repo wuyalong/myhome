bd477e1e0c7ca34ab060a8ecbf2ed4f9ecbe2011
wanghuina <18810129962@163.com>
2016-06-20 21:46:33 +0800

    many  add

--- frontend/controllers/UscoreController.php
+++ frontend/controllers/UscoreController.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace frontend\controllers;
-
+use yii;
+use frontend\models\Sort;
+header("Content-type: text/html; charset=utf-8");
 class UscoreController extends \yii\web\Controller
 {
-	public $layout=false;
+	public $layout="main";
     public function actionIndex()
     {
-        return $this->render('user_score');
+        $session = Yii::$app->session;
+        $id=$session->get('id');
+        $model=yii::$app->db->createCommand("select * from user where user_id='$id'")->queryOne();
+        $sql=yii::$app->db->createCommand("select * from `order` where user_id='$id'")->queryAll();
+        //print_r($sql);die;
+        //主菜单
+        $model1=new Sort();
+        $sort1=$model1->sel_sort();
+        $lists=new Sort();
+        $sorts=$lists->sel_sorts();
+        return $this->render('user_score',['res'=>$sql,'data'=>$model,'sort'=>$sort1,'sorts'=>$sorts]);
     }
 
 }
